4d22e8ea1183f8dcc9aa9296c6600b66cc1ee986
Kimmo Pekkola <kimmo.pekkola@gmail.com>
2017-10-28 17:34:35 +0300

    Project updates

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
-var css = require('./hello.styl');
+import * as css from "./hello.styl";
 
 export interface HelloProps { compiler: string; framework: string; }
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
+        console.log(css.hello);
         return (
             <div className={css.hello}>
                 <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as _ from "lodash";
 
 import { Hello } from "./components/Hello";
 import { World } from "./components/World";
+
+function rootElement() {
+    const element = document.createElement('div');
+    element.id = "root";
+    return element;
+}
+  
+document.body.appendChild(rootElement());
 
 ReactDOM.render(
     <div>
